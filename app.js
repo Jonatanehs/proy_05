29e062a1fd0ddfc16f8df00b096c0f1a9d76d056
Jonatanehs <jehernandez257@soy.sena.edu.co>
2023-08-22 10:02:34 -0500

    se inserta una rama

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require ('colors')
 
 
+const { nombre } = require('./proy_modules/text.js');
 const texto = require ('./proy_modules/text.js')
 
 console.clear();
@@ -9,3 +10,13 @@ console.log(texto.nombre("JOnaTan" , "hernAnDEz"));
 console.log(texto.nombreMayus("jonatan"));
 console.log(texto.nombreMinus("ESTIVEN"));
 console.log(texto.cantidad("Jonatan", "salazar"));
+
+
+ let i = nombre.length;
+ let y = 1;
+
+ while (i > 0) {
+    console.log(y  + '  ' + nombre.substring(0,i));
+    i--;
+    y++;
+ }
