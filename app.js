7379d24a74783d03e16cb30e774c1cee72ed528b
Jonatanehs <jehernandez257@soy.sena.edu.co>
2023-12-12 11:38:25 +1300

    se termina de comentarear el codigo

--- app.js
+++ app.js
@@ -1,38 +1,63 @@
+// se importa la biblioteca de colores de node js 
 require ('colors')
 
 
-const { nombre } = require('./proy_modules/text.js');
+// se importa en texto todo lo que se exporto en el modulo text.js
 const texto = require ('./proy_modules/text.js')
 
+// se limpia la consola 
 console.clear();
 
+// se imprimen mensajes en consola llamando a las funciones
 console.log(texto.nombre("JOnaTan" , "hernAnDEz"));
 console.log(texto.nombreMayus("jonatan"));
 console.log(texto.nombreMinus("ESTIVEN"));
 console.log(texto.cantidad("Jonatan", "salazar"));
 
-
+// se crean dos nuevas variables que contienen un string cada una 
 let nombre1 = "Estiven";
 let nombre2 = "Salazar";
 
+// se crea una nueva variable que contenga las dos anteriores variables 
 let nombres = nombre1 + "  " + nombre2;
 
+// se imprime lo que contenga la variable nombres 
 console.log(nombres);
 
+// se crea una variable llamada i que contendra la cantidad de caracteres de nombres 
  let i = nombres.length;
+
+//  se crea una variable llamada y que obtendra un 1 
  let y = 1;
 
+// se crea un bucle que se ejecute mientras i sea mayor que 0
  while (i > 0) {
+
+// se imprime un mensaje en consola que va a tener a nombres de 0 hasta i 
     console.log(y  + '  ' + nombres.substring(0,i));
+
+// i decrementa una en una
     i--;
+
+// y aumenta uno en uno 
     y++;
  }
 
+// i obtendra un valor en 0 
 i = 0;
+
+// y tendra el valor de 1 
 y = 1;
 
+// se creara un bucle que se ejecuta mientras i sea menor que la cantidad de nombres 
 while (i < nombres.length) {
+
+// se imprimira un mensaje donde estara lo obtenido en y y nombres que comienza desde i 
     console.log(y + '  ' + nombres.substring(i + 0));
+
+// i aumenta uno en uno 
     i++;
+
+// y aumenta uno en uno 
     y++;
 } 
--- proy_modules/text.js
+++ proy_modules/text.js
@@ -1,28 +1,49 @@
+// se crea una constante llamada texto que contiene un objeto vacio 
 const texto = {}
 
+/* se utiliza una nueva funcion llamada nombre y tendra como parametros
+a y b */
 function nombre (a,b) {
 
+/* se crea una nueva variable que contiene a a y su primer caracter se obtendra en 
+mayuscula, los caracteres apartir del caracter desde la posicion 1 sera en minuscula.  */
     let nombreCompleto = a[0].toUpperCase() + a.substring(1).toLowerCase() +
+
+/* el caracter que se encuentre en la posicion 0 sera en mayuscula y los caracteres que
+se encuentren en la posicion 1 sera en minuscula */
         ' ' + b[0].toUpperCase() + b.substring(1).toLowerCase()
+
+// se retornara lo que esta en nombreCompleto 
         return nombreCompleto;
 
 }
 
+// se crea una nueva funcion con nombre nombreMayus y tendra como parametro name 
 function nombreMayus (name) {
+
+// se retornara name en mayusculas
     return name.toUpperCase();
 }
 
+// se crea una funcion llamada nombreMinus y tendra como parametro name 
 function nombreMinus(name) {
+
+// se retorna name en minusculas
     return name.toLowerCase();
 }
 
+// se crea una funcion llamada cantidad y tendra como parametros c y d 
 function cantidad (c,d) {
+
+// se retornara el paramentro c y d que tendra la cantidad de caracteres 
     return (c).length  + (d).length;
 } 
 
+// texto.nombre contendra lo que esta en nombre y en las otras funciones
 texto.nombre = nombre;
 texto.nombreMayus = nombreMayus;
 texto.nombreMinus = nombreMinus;
 texto.cantidad = cantidad;
 
+// se exporta el modulo que contiene texto 
 module.exports = texto
